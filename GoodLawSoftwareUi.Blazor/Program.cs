9068b7f86ae72257a4526638f269e3db8f54ce62
Armia Fekry <Armia.Fekry@InteractTS.com>
2022-05-02 15:46:06 +0200

    Add project files.

---
+++ GoodLawSoftwareUi.Blazor/Program.cs
@@ -0,0 +1,13 @@
+using GoodLawSoftwareUi.Blazor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMudServices();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5193") });
+
+await builder.Build().RunAsync();
